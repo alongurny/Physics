f193a99460c5ba987f35ca65d6c8e1c11953473f
Alon Gurny <alongurny@gmail.com>
2016-04-08 20:33:04 +0200

    Removed unused things

--- Physics/src/physics/ArrayComprehension.java
+++ Physics/src/physics/ArrayComprehension.java
@@ -4,22 +4,12 @@ import java.util.function.Function;
 
 public class ArrayComprehension {
 
-	@SuppressWarnings("unchecked")
 	public static <T> Scalar[] get(T[] arr, Function<T, Scalar> f) {
 		Scalar[] ss = new Scalar[arr.length];
 		for (int i = 0; i < ss.length; i++) {
 			ss[i] = f.apply(arr[i]);
 		}
 		return ss;
-	}
-
-	@SuppressWarnings("unchecked")
-	public static <S> S[] get(Vector arr, Function<Scalar, S> f) {
-		Object[] ss = new Object[arr.getLength()];
-		for (int i = 0; i < ss.length; i++) {
-			ss[i] = f.apply(arr.get(i));
-		}
-		return (S[]) ss;
 	}
 
 	public static Scalar[] get(int length, Function<Integer, Scalar> f) {
