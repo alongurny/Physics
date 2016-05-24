6e4ec7895144d88ef3db6932ac4529ebc31c8f05
Alon Gurny <alongurny@gmail.com>
2016-05-24 19:56:12 +0300

    A little optimization

--- Physics/src/physics/body/MomentsOfInertia.java
+++ Physics/src/physics/body/MomentsOfInertia.java
@@ -1,12 +1,11 @@
 package physics.body;
 
-import physics.math.Mathx;
 import physics.math.Matrix;
 import physics.math.Scalar;
 
 public class MomentsOfInertia {
 	public static Matrix ball(int radiusDimension, Scalar mass, Scalar radius) {
-		int dimension = Mathx.choose(radiusDimension, 2);
+		int dimension = radiusDimension * (radiusDimension + 1) / 2;
 		Scalar i = mass.multiply(0.4).multiply(radius.divide(Scalar.RADIAN).pow(2));
 		return Matrix.identity(dimension).multiply(i);
 	}
